67fbd14833d6697474c7e6a881fc158ea68c11a9
by22dgb <by22dgb@gmail.com>
2022-11-11 14:16:46 +0800

    补充

--- chs.js
+++ chs.js
@@ -927,6 +927,8 @@ var cnItems = {
     'Don’t worry, new mechanics will arrive for you!\n        ': '别担心，新机制就要来了！',
     'Cool': '酷毙啦',
     'Entropy is unlocked in Quantum tab!': '量子选项卡下解锁了熵！',
+    'Yes': '是',
+    'No': '否',
     'Incremental Mass Rewritten v0.6 final release - made by MrRedShark77': '质量增量重制版v0.6 最终发布版 - 作者为MrRedShark77，由by22dgb汉化',
     'The game is inspired by Distance Incremental & Synergism': '本游戏灵感来自于距离增量及协同放置',
     'Hint 1: Hover top image above tabs to show description...': '提示1：鼠标停留在选项卡上方的图片以查看详情……',
